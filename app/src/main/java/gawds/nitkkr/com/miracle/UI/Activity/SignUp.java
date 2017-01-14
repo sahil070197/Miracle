f1c0f0b1733c7904fcb22f78e2113323e3fdfbd6
palakgarg2107 <sahil.070197@gmail.com>
2017-01-14 18:22:55 +0530

    SIGN UP SCREEN

--- app/src/main/java/gawds/nitkkr/com/miracle/UI/Activity/SignUp.java
+++ app/src/main/java/gawds/nitkkr/com/miracle/UI/Activity/SignUp.java
@@ -2,14 +2,40 @@ package gawds.nitkkr.com.miracle.UI.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import gawds.nitkkr.com.miracle.R;
 
-public class SignUp extends AppCompatActivity {
-
+public class SignUp extends AppCompatActivity  {
+    LinearLayout studentLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        studentLayout=(LinearLayout) findViewById(R.id.Student);
+        final Spinner spinner=(Spinner) findViewById(R.id.signUpType);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int pos=spinner.getSelectedItemPosition();
+                if(pos!=0)
+                {
+                    studentLayout.setVisibility(View.GONE);
+                }
+                else
+                {
+                    studentLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
