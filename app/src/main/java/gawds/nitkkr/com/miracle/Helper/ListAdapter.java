e0faf1691f042baa19a8413a904b18c55fbf1f59
palakgarg2107 <sahil.070197@gmail.com>
2017-01-15 00:14:51 +0530

    All three drwaers and remaining subject details fragment part

---
+++ app/src/main/java/gawds/nitkkr/com/miracle/Helper/ListAdapter.java
@@ -0,0 +1,51 @@
+package gawds.nitkkr.com.miracle.Helper;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.List;
+
+import gawds.nitkkr.com.miracle.R;
+
+/**
+ * Created by SAHIL SINGLA on 14-01-2017.
+ */
+public class ListAdapter extends ArrayAdapter<String> {
+    public ListAdapter(Context context,int textViewResourceId){
+        super(context,textViewResourceId);
+    }
+    public ListAdapter(Context context, int resource, List<String> items) {
+        super(context, resource, items);
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        View v = convertView;
+        if(v==null)
+        {
+            LayoutInflater vi;
+            vi=LayoutInflater.from(getContext());
+            v=vi.inflate(R.layout.attendance_list_item,null);
+        }
+        String student=getItem(position);
+        if(student!= null)
+        {
+            TextView name=(TextView) v.findViewById(R.id.name);
+            name.setText(student);
+            Switch present=(Switch) v.findViewById(R.id.present);
+            present.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Log.v("State",""+b);
+                }
+            });
+        }
+        return  v;
+    }
+}
--- app/src/main/java/gawds/nitkkr/com/miracle/UI/Activity/TakeAttendance.java
+++ app/src/main/java/gawds/nitkkr/com/miracle/UI/Activity/TakeAttendance.java
@@ -2,14 +2,22 @@ package gawds.nitkkr.com.miracle.UI.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gawds.nitkkr.com.miracle.R;
 
 public class TakeAttendance extends AppCompatActivity {
-
+    ListView studentList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_attendance);
+        studentList=(ListView) findViewById(R.id.attendanceList);
+        ArrayList<String> objectArray=new ArrayList<>();
+        ListAdapter customAdapter= new gawds.nitkkr.com.miracle.Helper.ListAdapter(this,R.layout.attendance_list_item,objectArray);
     }
 }
