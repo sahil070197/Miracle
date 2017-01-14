956dc7acfe59ca416c5a6e8b6c11ce98368bc07a
palakgarg2107 <sahil.070197@gmail.com>
2017-01-14 17:14:44 +0530

    Login and splash

---
+++ app/src/main/java/gawds/nitkkr/com/miracle/UI/Activity/SignUp.java
@@ -0,0 +1,15 @@
+package gawds.nitkkr.com.miracle.UI.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gawds.nitkkr.com.miracle.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
